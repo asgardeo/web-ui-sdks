c76ee31a0417d9a2857d749b3a8279958660fb0b
Brion Mario <info@brionmario.com>
2024-04-25 12:22:44 +0530

    chore: fix formatting issue

--- packages/core/src/api/authenticate.ts
+++ packages/core/src/api/authenticate.ts
@@ -60,6 +60,7 @@ const authenticate = async (props: AuthApiRequestBody): Promise<AuthApiResponse>
   if (response.ok) {
     return (await response.json()) as AuthApiResponse;
   }
+
   throw new AsgardeoUIException(
     'JS_UI_CORE-AUTHN-AN-HE',
     'Failed to receive a successful response from the authentication endpoint',
