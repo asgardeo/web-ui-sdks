353b2efd09604cc8e380e2366f5ee1a5bbef564a
Movin Silva <movinpinsara@gmail.com>
2024-05-06 16:38:12 +0530

    refactor(core): :recycle: rename branding-preference-text file to get-branding-preference-text

--- packages/core/src/api/public-api.ts
+++ packages/core/src/api/public-api.ts
@@ -20,4 +20,4 @@ export {default as authorize} from './authorize';
 export {default as authenticate} from './authenticate';
 export {default as getBrandingPreference} from './branding-preference';
 export {default as getProfileInformation} from './profile';
-export {default as getBrandingPreferenceText} from './branding-preference-text';
+export {default as getBrandingPreferenceText} from './get-branding-preference-text';
