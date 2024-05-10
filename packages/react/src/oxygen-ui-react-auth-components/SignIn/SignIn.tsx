e6fc1656a18bbb099035c9be3ee9626f715fa83d
Movin Silva <movinpinsara@gmail.com>
2024-05-10 06:42:03 +0530

    feat(auth-components): :sparkles: update SignIn component

--- packages/react/src/oxygen-ui-react-auth-components/SignIn/SignIn.tsx
+++ packages/react/src/oxygen-ui-react-auth-components/SignIn/SignIn.tsx
@@ -21,9 +21,11 @@ import clsx from 'clsx';
 import {ElementType, ForwardRefExoticComponent, MutableRefObject, ReactElement, forwardRef} from 'react';
 import {WithWrapperProps} from '../models/component';
 import SignInAlert from '../SignInAlert/SignInAlert';
+import SignInButton from '../SignInButton/SignInButton';
 import SignInDivider from '../SignInDivider/SignInDivider';
 import SignInLink from '../SignInLink/SignInLink';
 import SignInPaper from '../SignInPaper/SignInPaper';
+import SignInTextField from '../SignInTextField/SignInTextField';
 import SignInTypography from '../SignInTypography/SignInTypography';
 import pascalCaseToKebabCase from '../utils/pascal-case-to-kebab-case';
 
@@ -33,9 +35,11 @@ export type SignInProps<C extends ElementType = ElementType> = {
 
 type SignInCompoundProps = {
   Alert: typeof SignInAlert;
+  Button: typeof SignInButton;
   Divider: typeof SignInDivider;
   Link: typeof SignInLink;
   Paper: typeof SignInPaper;
+  TextField: typeof SignInTextField;
   Typography: typeof SignInTypography;
 };
 
@@ -59,5 +63,7 @@ SignIn.Paper = SignInPaper;
 SignIn.Alert = SignInAlert;
 SignIn.Divider = SignInDivider;
 SignIn.Link = SignInLink;
+SignIn.Button = SignInButton;
+SignIn.TextField = SignInTextField;
 
 export default SignIn;
