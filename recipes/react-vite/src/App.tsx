c2d66f0ec0e6af5bfe7975f26720557c6b06bfe8
Movin Silva <movinpinsara@gmail.com>
2024-05-15 08:48:18 +0530

    feat(sample-app): :package: add components to sample app

--- recipes/react-vite/src/App.tsx
+++ recipes/react-vite/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { SignIn } from "../../../packages/react/src"; // ToDO: temporary
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <SignIn />
+    </>
+  );
 }
 
 export default App;
--- recipes/react-vite/src/main.tsx
+++ recipes/react-vite/src/main.tsx
@@ -1,9 +1,20 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import React from "react";
+import { AsgardeoProvider, UIAuthConfig } from "../../../packages/react/src"; //TODO: temporary
+
+const config: UIAuthConfig = {
+  baseUrl: "https://localhost:9443",
+  clientID: "b1uRjwpqydvxjGR42Y6BnIdQMRMa",
+  scope: ["openid", "internal_login", "profile"],
+  signInRedirectURL: "https://localhost:5173",
+  enableConsoleTextBranding: true,
+};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AsgardeoProvider config={config}>
+      <App />
+    </AsgardeoProvider>
   </React.StrictMode>
 );
