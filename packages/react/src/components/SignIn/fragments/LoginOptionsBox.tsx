0ee03ebe356944604cef4f3abe31984520805f71
Movin Silva <movinpinsara@gmail.com>
2024-05-22 10:48:46 +0530

    feat(react): :lipstick: add unique class names for social login buttons

--- packages/react/src/components/SignIn/fragments/LoginOptionsBox.tsx
+++ packages/react/src/components/SignIn/fragments/LoginOptionsBox.tsx
@@ -53,6 +53,7 @@ const LoginOptionsBox = ({
   handleOnClick,
 }: LoginOptionsBoxProps): ReactElement => (
   <UISignIn.Button
+    className={`login-options-btn-${socialName.toLowerCase()}`}
     social
     startIcon={<img className="social-login-img" src={images[socialName]} alt={socialName} />}
     onClick={handleOnClick}
