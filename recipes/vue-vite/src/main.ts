43f38b3b9f146439c444d5b0ad8e97a1e2448bf3
3nethz <ggthineth@gmail.com>
2025-03-20 11:55:46 +0530

    chore(sample-app): change configuration file

--- recipes/vue-vite/src/main.ts
+++ recipes/vue-vite/src/main.ts
@@ -28,8 +28,8 @@ const app: VueApp = createApp(App)
 const config: AuthVueConfig = {
   baseUrl: import.meta.env.VITE_ASGARDEO_BASE_URL,
   clientID: import.meta.env.VITE_ASGARDEO_CLIENT_ID,
-  disableTrySignInSilently: import.meta.env.VITE_ASGARDEO_DISABLE_TRY_SIGN_IN_SILENTLY,
-  scope: import.meta.env.VITE_ASGARDEO_SCOPE.split(','),
+  disableTrySignInSilently: false,
+  scope: ['openid', 'profile', 'email'],
   signInRedirectURL: import.meta.env.VITE_ASGARDEO_SIGN_IN_REDIRECT_URL,
   signOutRedirectURL: import.meta.env.VITE_ASGARDEO_SIGN_OUT_REDIRECT_URL,
 }
