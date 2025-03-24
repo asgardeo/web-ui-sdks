39c3055a86769478faaec92e79d1cd8dc35045dc
3nethz <ggthineth@gmail.com>
2025-03-24 15:46:23 +0530

    chore(vue): change package.json and eslint config

--- packages/vue/.eslintrc.cjs
+++ packages/vue/.eslintrc.cjs
@@ -21,7 +21,6 @@ const path = require('path');
 module.exports = {
   extends: [
     'plugin:@wso2/typescript',
-    // 'plugin:@wso2/vue',
     'plugin:@wso2/strict',
     'plugin:@wso2/internal',
     'plugin:@wso2/prettier',
