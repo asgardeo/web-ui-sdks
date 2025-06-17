ecdc6d68b474b30c7c56e1f140e6cc189b5dee92
Brion <info@brionmario.com>
2025-06-18 02:57:16 +0530

    chore: add `afterSignOutUrl`

--- samples/teamspace-react/src/main.tsx
+++ samples/teamspace-react/src/main.tsx
@@ -9,6 +9,7 @@ createRoot(document.getElementById('root')!).render(
     <AsgardeoProvider
       baseUrl={import.meta.env.VITE_ASGARDEO_BASE_URL}
       afterSignInUrl={import.meta.env.VITE_ASGARDEO_AFTER_SIGN_IN_URL}
+      afterSignOutUrl={import.meta.env.VITE_ASGARDEO_AFTER_SIGN_OUT_URL}
       clientId={import.meta.env.VITE_ASGARDEO_CLIENT_ID}
       scopes={['openid', 'address', 'email', 'profile']}
       preferences={{
